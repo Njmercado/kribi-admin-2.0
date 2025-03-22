f10a7ba99b5d9a9f81e4613b364ddd22e321055f
Nino Mercado <njesusmercado@gmail.com>
2025-03-22 14:36:54 -0500

    refact(WordDrawer): organized onSubmit callback

--- src/components/molecules/word_drawer/index.tsx
+++ src/components/molecules/word_drawer/index.tsx
@@ -1,12 +1,13 @@
+import { SUBMIT_ACTIONS, SubmitAction } from '@/contants';
 import { Drawer } from '../../atom'
 import { DrawerDirection } from '../../atom/drawer'
-import { WordDTO } from "@/models";
+import { SpanishWordDTO, WordDTO } from "@/models";
 
 export interface WordDrawerProps {
   isOpen: boolean;
   onClose: () => void;
   data: WordDTO;
-  onSubmit?: (form: WordDTO) => void;
+  onSubmit?: (form: SpanishWordDTO, action: SubmitAction) => void;
 }
 
 export default function WordDrawer({
@@ -26,14 +27,17 @@ export default function WordDrawer({
     const definitions = formData.get('definitions') as string;
     const examples = formData.get('examples') as string;
     if (onSubmit) {
-      onSubmit({
-        id: data.id,
-        word,
-        type,
-        translations: translations.split(','),
-        definitions: definitions.split(','),
-        examples: examples.split(','),
-      });
+      onSubmit(
+        {
+          _id: data._id as string,
+          palabra: word,
+          tipo: type,
+          traducciones: translations.split(','),
+          definicion: definitions.split(','),
+          ejemplos: examples.split(','),
+        },
+        data._id == -1 ? SUBMIT_ACTIONS.ADD : SUBMIT_ACTIONS.UPDATE
+      );
     }
   }
 
@@ -47,11 +51,11 @@ export default function WordDrawer({
         <section className="flex flex-col gap-2 p-2">
           <h2 className="text-2xl font-bold text-center">Add Word</h2>
           <form method="post" className="flex flex-col gap-2" onSubmit={handleOnSubmit}>
-            <input type="text" name="word" placeholder="Word" className="rounded-md border-solid border-black border-2" defaultValue={data.word}/>
-            <input type="text" name="type" placeholder="Type" className="rounded-md border-solid border-black border-2" defaultValue={data.type}/>
-            <input type="text" name="translations" placeholder="Translations" className="rounded-md border-solid border-black border-2" defaultValue={data.translations.join(',')}/>
-            <input type="text" name="definitions" placeholder="Definitions" className="rounded-md border-solid border-black border-2" defaultValue={data.definitions.join(',')}/>
-            <input type="text" name="examples" placeholder="Examples" className="rounded-md border-solid border-black border-2" defaultValue={data.examples.join(',')}/>
+            <input type="text" name="word" placeholder="Word" className="rounded-md border-solid border-black border-2" defaultValue={data.word} />
+            <input type="text" name="type" placeholder="Type" className="rounded-md border-solid border-black border-2" defaultValue={data.type} />
+            <input type="text" name="translations" placeholder="Translations" className="rounded-md border-solid border-black border-2" defaultValue={data.translations.join(',')} />
+            <input type="text" name="definitions" placeholder="Definitions" className="rounded-md border-solid border-black border-2" defaultValue={data.definitions.join(',')} />
+            <input type="text" name="examples" placeholder="Examples" className="rounded-md border-solid border-black border-2" defaultValue={data.examples.join(',')} />
             <button formAction='submit' className="rounded-md bg-blue-400 p-2 text-white font-bold">Add Word</button>
           </form>
         </section>
