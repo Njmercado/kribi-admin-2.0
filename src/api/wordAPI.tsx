e57b39c96a1fdf3590bcda073a78d2d8c88144d6
Nino Mercado <njesusmercado@gmail.com>
2026-03-08 10:46:32 -0500

    feat: add optional limit and page parameters to the word search API call.

--- src/api/wordAPI.tsx
+++ src/api/wordAPI.tsx
@@ -6,9 +6,9 @@ import { PREFIXES, API_ENDPOINTS } from "@/contants";
 
 const customFetch = new CustomFetch(PREFIXES.WORD);
 
-export function search(word: string) {
+export function search(word: string, limit: number = 10, page: number = 1) {
   const encodedWord = encodeURIComponent(word);
-  return customFetch.get<Array<WordDTO>>({ path: `${API_ENDPOINTS.WORD.SEARCH}?word=${encodedWord}` });
+  return customFetch.get<Array<WordDTO>>({ path: `${API_ENDPOINTS.WORD.SEARCH}?word=${encodedWord}&limit=${limit}&page=${page}` });
 }
 
 export function update(word: WordDTO) {
